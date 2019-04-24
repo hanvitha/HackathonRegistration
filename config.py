0ab265dc6c8b6bce1a4ccb61186c0aef76654c32
Hanvitha Gavini <gavini.honey@gmail.com>
2019-04-24 16:53:33 -0500

    added gunicorn config

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '5'))
+threads = int(os.environ.get('GUNICORN_THREADS', '3'))
+timeout = 6000
+forwarded_allow_ips = '*'
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
