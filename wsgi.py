843516c4eb4b481360537f5c7c63e1e940f59b85
Hanvitha Gavini <gavini.honey@gmail.com>
2019-04-26 11:05:11 -0500

    whatever

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,7 @@ __author__ = 'hanvitha'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 # APP_ROOT = "/opt/app-root/src/aahack"
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# host="mysql.registration.svc"
 host="mysql.registration.svc"
 user="root"
 password="reg_user"
@@ -20,14 +21,6 @@ db = mysql.connector.connect(host=host,
                              database=database
                             )
 cursor = db.cursor()
-# mysql = MySQL()
-# MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'reg_user'
-# app.config['MYSQL_DATABASE_DB'] = 'reg_db'
-# app.config['MYSQL_DATABASE_HOST'] = 'mysql.registration.svc'
-# mysql.init_app(app)
-# https://mysql-registration.apps.aahw6prep-7559.openshiftworkshop.com
 
 u = Util
 @app.route("/", methods=["GET", "POST"])
@@ -46,7 +39,7 @@ def save():
     except Exception as e:
         print(json.dumps({"error": str(e)}))
         return "<h1>Oops! Something went wrong.. Could you try after sometime or reach out to the host!</h1>"
-
+#
 @app.route("/users", strict_slashes=False)
 def users():
     return render_template("users.html")
@@ -62,15 +55,16 @@ def usersall(status=None):
         cursor.execute('''select * from users where status=0''')
 
     allusers = cursor.fetchall()
+    print(allusers)
     return render_template("usersRegistered.html", users=allusers, host=host,user=user, password=password, database=database)
 
-
-@app.route("/updatestatus", methods=["POST"])
-def updatestatus():
-    uid = request.args.get('id')
-    status = request.args.get('status')
-    cursor.execute('''update users set status=%s where uid=%s''', (status,uid))
-    return "done"
+#
+# @app.route("/updatestatus", methods=["POST"])
+# def updatestatus():
+#     uid = request.args.get('id')
+#     status = request.args.get('status')
+#     cursor.execute('''update users set status=%s where uid=%s''', (status,uid))
+#     return "done"
 
 if __name__ == '__main__':
     app.run()
