e1f1ba9ad8c608d219783a63a6e0c0ef5bdfb1f1
Hanvitha Gavini <gavini.honey@gmail.com>
2019-05-03 14:21:29 -0400

    F

--- utils.py
+++ utils.py
@@ -14,7 +14,9 @@ class Utility:
             team = request.form['team']
             role = request.form['role']
             redhatid = request.form['redhatid']
-            print(fname)
+            if redhatid == "":
+                redhatid="Not Provided"
+
             uid = ""+fname[:1]+lname
             uid = uid.lower()
             cursor.execute('''select * from users where uid = %s''', (uid,))
@@ -24,7 +26,7 @@ class Utility:
             sql_insert_query ='''INSERT INTO users(uid,fname,lname, email,phone, team, redhatid, role) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)'''
             insert_tuple = (uid,fname,lname, email,phone, team, redhatid, role)
             print("Trying to insert! ", sql_insert_query)
-            print("Connection ",conn)
+            # print("Connection ",conn)
             cursor.execute(sql_insert_query, insert_tuple)
             conn.commit()
             print(cursor.rowcount, " record inserted.")
